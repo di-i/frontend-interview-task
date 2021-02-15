fee08566724d7c61f7679c5fa2a1184d82f45134
di-i <55539864+di-i@users.noreply.github.com>
2021-02-15 12:43:49 +0300

    Added NoResults component and styled for it

---
+++ src/components/common/noResults/NoResults.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyledButton } from '../list/styled';
+import { StyledText, Wrapper } from './styled';
+import PropTypes from 'prop-types';
+
+export const NoResults = (props) => {
+  return (
+    <Wrapper>
+      <StyledText>{props.text}</StyledText>
+      <StyledButton onClick={() => window.location.reload()} type='primary'>
+        Reset
+      </StyledButton>
+    </Wrapper>
+  );
+};
+
+NoResults.propTypes = {
+  text: PropTypes.string.isRequired,
+};
---
+++ src/components/common/noResults/styled.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import { colors } from '../../constants/colors';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 150px;
+`;
+
+export const StyledText = styled.div`
+  margin-bottom: 30px;
+
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 22px;
+  color: ${colors.fontSecond};
+`;
