6681cc2e16e594dfa8ea46fd9c5c40e1ea95c8fb
di-i <55539864+di-i@users.noreply.github.com>
2021-02-15 11:43:20 +0300

    Added global styles and routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Dashboard } from './components/pages/Dashboard';
+import { Finalize } from './components/pages/Finalize';
+import { Results } from './components/pages/Results';
+import { GlobalStyle, Wrapper } from './styled';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <GlobalStyle />
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Dashboard} />
+        </Switch>
+        <Route exact path='/results/:id' component={Results} />
+        <Route exact path='/finalize/:id' component={Finalize} />
+      </Router>
+    </Wrapper>
   );
-}
-
-export default App;
+};
