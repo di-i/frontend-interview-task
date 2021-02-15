327ca19f6bc4d0fa0a93d0dfd855299f1a87a1a8
di-i <55539864+di-i@users.noreply.github.com>
2021-02-15 12:41:16 +0300

    Added visible items counter for search bar

--- src/components/common/counter/Counter.js
+++ src/components/common/counter/Counter.js
@@ -2,5 +2,5 @@ import React from 'react';
 import { StyledCounter } from './styled';
 
 export const Counter = (props) => {
-    return <StyledCounter>{`${props.items} ${props.string}`}</StyledCounter>
+    return <StyledCounter>{`${props.itemsLength} ${props.counterText}`}</StyledCounter>
 }
