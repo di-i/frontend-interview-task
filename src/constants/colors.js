dda942589a851b79c5d7c67a5163cb0ddf09ca72
di-i <55539864+di-i@users.noreply.github.com>
2021-02-15 13:42:48 +0300

    Added colors component

---
+++ src/constants/colors.js
@@ -0,0 +1,23 @@
+export const colors = {
+    fontMain: '#5C5C5C',
+    fontSecond: '#222222',
+
+    lightGrey: '#D1D1D1',
+    backLayout: '#fafafa',
+
+    colorDarkGray: '#999999',
+    colorTableName: '#474747',
+
+    primaryButton: '#2EE5AC',
+    secondButton: '#7D7D7D',
+
+    statusGreen: '#1BDA9D',
+    statusOrange: '#FF8346',
+    statusRed: '#FE4848',
+
+    borderIndianRed: '#E14165',
+    borderMediumPurple: '#8686FF',
+    borderLavender: '#C2C2FF',
+
+    white: '#ffffff'
+}
