a1569f2249c9d38c1b8f53ef46e7cead0b83f3d7
di-i <55539864+di-i@users.noreply.github.com>
2021-02-15 12:41:46 +0300

    Added styled for visible items counter

---
+++ src/components/common/counter/styled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+import { colors } from '../../../constants/colors';
+
+
+export const StyledCounter = styled.div`
+  position: absolute;
+  right: 16.5px;
+  bottom: 15px;
+
+  font-family: Roboto, sans-serif;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 14px;
+  line-height: 16px;
+  letter-spacing: 1.2px;
+
+  color: ${colors.lightGrey}
+`;
